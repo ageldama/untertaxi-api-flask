97c399e5b52f97e72dfe1917605405c189a903ed
Jonghyouk Yun <ageldama@gmail.com>
2018-05-19 18:50:29 +0900

    some scripts

---
+++ misc/connect-docker-pg.sh
@@ -0,0 +1,2 @@
+PG_PASSWORD=postgres docker exec -it postgres psql -Upostgres untertaxi_api $*
+
---
+++ misc/docker-run-postgres10.sh
@@ -0,0 +1,2 @@
+docker run --name postgres -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres postgres:10-alpine
+
