03fb5682e973ab50dad6f6978b551f9a223c4c18
Jonghyouk Yun <ageldama@gmail.com>
2018-05-19 18:04:37 +0900

    WIP: db -- `MemberAddress`

--- untertaxi_api/config.py
+++ untertaxi_api/config.py
@@ -7,7 +7,7 @@ Configuration object definition.
 class LocalhostSettings(object):
     """Default Configuration"""
     SECRET_KEY = 'default-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///untertaxi.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:postgres@127.0.0.1:5432/untertaxi_api'
     TESTING = False
     DEBUG = True
 
